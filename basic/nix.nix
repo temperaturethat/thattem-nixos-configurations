4393d89c4807cb0bebdcd5cd82d6df535e5184ae
thattemperature <2719023332@qq.com>
2026-05-27 12:01:41 +0800

    Update "flake.lock" and add github-token setting.

--- basic/nix.nix
+++ basic/nix.nix
@@ -3,6 +3,7 @@
 {
 
   config = lib.mkMerge [
+
     {
       nix.settings.experimental-features = [
         "nix-command"
@@ -15,8 +16,18 @@
       nix.settings = {
         substituters = config.thattem.private.nix-serve.substituters;
         trusted-public-keys = config.thattem.private.nix-serve.trusted-public-keys;
+
       };
     })
+
+    (lib.mkIf config.thattem.nixos.special.enable (
+      lib.mkIf (config.thattem.secrets.github-token.path != null) {
+
+        nix.extraOptions = "!include ${config.thattem.secrets.github-token.path}";
+
+      }
+    ))
+
   ];
 
 }
